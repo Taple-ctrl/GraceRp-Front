a3efdf1ab942224d74f3b5d6088cca0ed1a98190
Pavel Somov <connor.enemy@mail.ru>
2022-07-21 09:11:33 +0300

    Create start project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <HudMain v-if="isHud"/>
+  <AuthMain v-if="isAuth"/>
+</template>
+
+<script>
+import HudMain from './interfaces/hud'
+import AuthMain from './interfaces/auth'
+
+export default {
+  name: 'App',
+  components: {
+    HudMain,
+    AuthMain
+  },
+  data(){
+    return {
+      isHud: false,
+      isAuth: true,
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+</style>
---
+++ src/interfaces/auth.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="AuthMain">
+    <div class="WelcomeServer" v-if="IsWelcome">
+      <div class="welcome_info"></div>
+      <div class="welcome_btn" v-on:click="IsWelcome = !IsWelcome">Начать свою историю</div>
+      <div class="welcome_hint">Нажмите Enter</div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'AuthMain',
+  components:{
+
+  },
+  data(){
+    return{
+      IsWelcome: true,
+      registrationModel: {
+        login: '',
+        email: '',
+        password: '',
+        password2: '',
+        promo: ''
+      },
+      authorizationModel: {
+        login: '',
+        password: ''
+      },
+      recoveryModel: {
+        email: ''
+      }
+    }
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<style>
+
+body{
+  background-image: url('../assets/images/auth/backgroundAuth.jpg');
+  background-attachment: fixed;
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.AuthMain{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+.WelcomeServer{
+  width: 30%;
+  height: 60%;
+}
+
+.welcome_info{
+  height: 80%;
+  background: black;
+}
+
+.welcome_btn{
+  color: white;
+  font-weight: 800;
+  font-size: 3.5vh;
+  background: deepskyblue;
+  height: 15%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+
+.welcome_hint{
+  color: gray;
+  text-align: center;
+  margin-top: .5em;
+}
+
+</style>
